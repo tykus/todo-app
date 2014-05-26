97f9c316f3f07d341901c2b87175fa6c619b9760
Brian O'Sullivan (osx) <brian@tykus.ie>
2014-05-26 19:45:34 +0800

    Creating actions and views for Project resource

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,28 +1,57 @@
 class ProjectsController < ApplicationController
+  
+  layout false
+
+  before_action :find_project, :only => [:show, :edit, :update, :delete, :destroy]
+
   def index
+    @projects = Project.all
   end
 
   def show
   end
 
   def new
+    @project = Project.new
   end
 
   def create
-    
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project successfully created."
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
   end
 
   def update
-    
+    if @project.update_attributes(project_params)
+      flash[:notice] = "Project successfully updated."
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
   end
 
   def delete
   end
 
   def destroy
-    
+    @project.delete
+    flash[:notice] = "Project successfully deleted."
+    redirect_to(:action => 'index')
   end
+
+  private
+    def find_project
+      @project = Project.find(params[:id])
+    end
+
+    def project_params
+      params.require(:project).permit(:title, :description, :due_date, :completed)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526065513) do
+ActiveRecord::Schema.define(version: 20140526082751) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140526065513) do
     t.boolean  "completed"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "project_id"
   end
 
 end
